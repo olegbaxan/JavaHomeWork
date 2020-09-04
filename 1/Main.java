16ab5a5ab1efbf2a780a3de14c7366561805006d
Oleg Baxan <oleg.baxan@gmail.com>
2020-09-04 14:58:10 +0300

    First initialization of java Home work folders

---
+++ 1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main (String[] args) {
+		int firstValue=2;
+		int secondValue;
+		secondValue=3;
+		int resultValue=firstValue+secondValue;
+		System.out.println(resultValue);
+		}
+}
---
+++ 2/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=5,b=4;
+		int valueSum=a+b;
+		int valueMinus=a-b;
+		int valueMultiply=a*b;
+
+		System.out.println(valueSum+" / "+valueMinus+" / "+valueMultiply);
+		int d=6;
+		
+		d=d+1;
+		System.out.println("d+1="+d);
+		d+=1;
+		System.out.println("+1="+d);
+		d++;
+		System.out.println("d++="+d);
+
+
+	}
+}
