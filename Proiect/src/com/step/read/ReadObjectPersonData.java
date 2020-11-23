62d61060d1346d0211086d46a791ae3e047b8595
olegbaxan <43432810+olegbaxan@users.noreply.github.com>
2020-11-23 15:50:06 +0200

    Some small corection to try catch for Object Stream File

--- Proiect/src/com/step/read/ReadObjectPersonData.java
+++ Proiect/src/com/step/read/ReadObjectPersonData.java
@@ -4,6 +4,7 @@ import com.step.person.Person;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 
 import static com.step.person.PersonDataManager.per;
@@ -11,21 +12,24 @@ import static com.step.person.PersonDataManager.per;
 public class ReadObjectPersonData {
     public static void ReadObjectPersonFromFile() {
         try {
-        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("person.dat"));
-        while (true) {
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("person.dat"));
+            try {
+                while (true) {
 
-                Object person = objectInputStream.readObject();
-                if (person == null) {
-                    break;
+                    Object person = objectInputStream.readObject();
+                    if (person == null) {
+                        break;
+                    }
+                    per.add((Person) person);
                 }
-                per.add((Person)person);
-            }
-            objectInputStream.close();
-        }
-        catch (Exception ex) {
+                objectInputStream.close();
+            }catch (IOException e){
                 System.out.println("Solved EOF");
+            }
+
+        } catch (Exception ex) {
+            System.out.println("No  person.dat found. Skipping importing the data");
 
         }
-
     }
 }
